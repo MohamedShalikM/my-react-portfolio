a38b2689059851830fbc7f13f4eb38eacec6f6ae
MohamedShalikM <mohamedshalik25.it@gmail.com>
2026-01-31 12:33:04 +0530

    changes in project,certifications

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -1,47 +1,62 @@
 import React from "react";
 import "../css/certifications.css";
+import Cert1 from '../assets/logos/nodejs.svg';
+import Cert2 from '../assets/logos/react.svg';
+import Cert3 from '../assets/logos/mongodb.svg';
+import Cert4 from '../assets/logos/html5.svg';
+import Cert5 from '../assets/logos/css3.svg';
+import Cert6 from '../assets/logos/js.svg';
+import Cert7 from '../assets/logos/python.svg';
+import Cert8 from '../assets/logos/django.svg';
 
 const certifications = [
   {
     title: "NodeJS Foundation Course",
     issuer: "ScholarHat",
     year: 2026,
+    img:Cert1,
     link: "https://drive.google.com/file/d/1GP4eepFQL2HrwfwE7L6EjZhsKUfq2Vic/view",
   },
   {
     title: "ReactJS Foundation Course",
     issuer: "ScholarHat",
     year: 2026,
+    img:Cert2,
     link: "https://drive.google.com/file/d/1tGy7FXrunBKgnhT1xHbOkEjQtGrT-EYK/view?usp=drive_link",
   },
   {
     title: "MongoDB Foundation Course",
     issuer: "ScholarHat",
     year: 2026,
+    img: Cert3,
     link: "https://drive.google.com/file/d/1KoD3Z2K_9Ff0c0CvxjlyOOJgcjwhKIbh/view?usp=drive_link",
   },
   {
     title: "HTML",
     issuer: "Infosys Springboard",
     year: "2024",
+    img:Cert4,
     link: "https://drive.google.com/file/d/1-hkvb4nappCmR87-AmwEzywJVkZmbZtf/view?usp=drive_link",
   },
   {
     title: "CSS",
     issuer: "Scaler Topics",
     year: "2025",
+    img:Cert5,
     link: "https://drive.google.com/file/d/1Z_Siu6ROPcknFg0L936RxcGaRMCIrEm9/view?usp=drive_link",
   },
   {
     title: "Javascript",
     issuer: "Infosys Springboard",
     year: 2024,
+    img:Cert6,
     link: "https://drive.google.com/file/d/1XRM5OgZTXsLlhv0Ja90WTofl6ghCHiRJ/view?usp=drive_link",
   },
   {
     title: "Python Programming",
     issuer: "Besant Technologies",
     year: "2025",
+    img:Cert7,
     link: "https://drive.google.com/file/d/1HPBWk_daGM_rtMagkyB1Jl7i5or3fGo9/view?usp=drive_link",
   },
 
@@ -49,6 +64,7 @@ const certifications = [
     title: "Full Stack Web Development",
     issuer: "Eagle HiTech SoftClou Pvt Ltd",
     year: "2025",
+    img:Cert1,
     link: "https://drive.google.com/file/d/1xanLw0ornFmtW_StSnNo0tK4WlHqxkxR/view?usp=drive_link",
   },
 
@@ -56,6 +72,7 @@ const certifications = [
     title: "Python Full Stack",
     issuer: "IPCS Global Institute",
     year: "2025",
+    img:Cert8,
     link: "https://drive.google.com/file/d/1I0idXlkZ6SfFMvJliB1dAmYP66Lmed-f/view?usp=drive_link",
   },
 ];
@@ -71,12 +88,12 @@ function Certifications() {
       <div className="cert-grid">
         {certifications.map((cert, index) => (
           <div className="cert-card" key={index}>
-            <a href={cert.link} target="_blank">
-              <div className="cert-badge">✔</div>
-              <h3>{cert.title}</h3>
-              <span className="cert-issuer">{cert.issuer}</span>
-              <span className="cert-year">{cert.year}</span>
-            </a>
+            <div className="cert-badge">✔</div>
+            <h3>{cert.title}</h3>
+            <img className="cert-img" src={cert.img} alt={cert.title} />
+            <span className="cert-issuer">{cert.issuer}</span>
+            <span className="cert-year">{cert.year}</span>
+            <a href={cert.link} target="_blank"><span>View Certificate</span></a>
           </div>
         ))}
       </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import "../css/project.css";
-
+import Project1 from '../assets/Portfolio.png';
+import Project2 from '../assets/Raftel Fashion.png';
+import Project3 from '../assets/Drivex Showroom.png';
+import Project4 from '../assets/Drivex Magazine.png';
 const projects = [
   {
     title: "Personal Portfolio Website",
     description: "Developed a dynamic personal portfolio using React for component-based architecture, HTML for semantic structure, and CSS for responsive styling across devices.Features include animated project galleries, skill progress bars, smooth scrolling navigation, and a contact form with form validation, all optimized for performance and accessibility.Highlights proficiency in React hooks, state management, modern CSS (Flexbox/Grid), and responsive design—ideal for frontend developer or UI/UX roles.",
+    img: Project1,
     tech: ["React","HTML","CSS", "JavaScript"],
     link: "#",
   },
   {
     title: "Raftel: Fashion E-Commerce App",
     description: "Developed a responsive fashion e-commerce website with React frontend (HTML/CSS/JavaScript) for dynamic product displays, Django backend, and SQLite database handling clothing listings, sizes, prices, shopping cart, and checkout processes.",
+    img:Project2,
     tech: ["React","Django","HTML","CSS","Javascript","SQLite"],
     link: "#",
   },
@@ -18,6 +23,7 @@ const projects = [
   {
      title: "DriveX: Interactive Car Showroom",
     description: "Developed a straightforward car showroom site using HTML, CSS, and JavaScript for the frontend, paired with Django backend and SQLite database to showcase car listings, models, and rates.",
+    img:Project3,
     tech: ["Django","HTML","CSS","Javascript","SQLite"],
     link: "#",
   },
@@ -25,6 +31,7 @@ const projects = [
   {
     title: "Drivex: 2025 Luxury Car Magazine",
     description: "Developed DriveX, a digital magazine exploring top luxury vehicles of 2025, including electric hypercars, performance SUVs, and bespoke designs from brands like Porsche and Ferrari.",
+    img:Project4,
     tech: ["HTML", "CSS"],
     link: "https://drivex-magazine.neocities.org/",
   },
@@ -43,6 +50,7 @@ function Project() {
         {projects.map((project, index) => (
           <div className="project-card" key={index}>
             <h3>{project.title}</h3>
+            <img className = "project-img" src={project.img} alt={project.title} />
             <p className="project-desc">{project.description}</p>
 
             <div className="tech-stack">
